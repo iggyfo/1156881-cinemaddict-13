8603f6fc6e8d6b04fc78ede7d880f5636fbde39d
iggyfo <iggyfo@gmail.com>
2020-11-22 16:08:28 +0300

    Добавляет модули, отрисовывает модули в index.html

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import {createSiteUserRankTemplate} from "./view/user-rank";
+import {createSiteMenuTemplate} from "./view/menu";
+import {createSiteSortTemplate} from "./view/sort";
+import {createSiteFilmsTemplate} from "./view/films";
+import {createSiteFilmsListTemplate} from "./view/films-list";
+import {createSiteFilmCardTemplate} from "./view/film-card";
+import {createSiteMoreButtonTemplate} from "./view/show-more-button";
+import {createSiteFilmsExtraTemplate} from "./view/films-extra";
+import {createSiteStatisticsTemplate} from "./view/statistics";
+import {createSiteDetailsTemplate} from "./view/details";
+
+const NUM_CARDS_OF_FILM = 5;
+const NUM_CARDS_OF_EXTRA_FILM = 2;
+
+const siteHeaderElement = document.querySelector(`.header`);
+const siteMainElement = document.querySelector(`.main`);
+const siteFooterElement = document.querySelector(`.footer`);
+
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+// const siteFooterElement = document.querySelector(`.footer`);
+
+render(siteHeaderElement, createSiteUserRankTemplate(), `beforeend`);
+render(siteMainElement, createSiteMenuTemplate(), `beforeend`);
+render(siteMainElement, createSiteSortTemplate(), `beforeend`);
+render(siteMainElement, createSiteFilmsTemplate(), `beforeend`);
+
+const siteFilmsElement = siteMainElement.querySelector(`.films`);
+render(siteFilmsElement, createSiteFilmsListTemplate(), `beforeend`);
+
+const siteFilmsListContainerElement = siteFilmsElement.querySelector(`.films-list__container`);
+for (let i = 0; i < NUM_CARDS_OF_FILM; i++) {
+  render(siteFilmsListContainerElement, createSiteFilmCardTemplate(), `beforeend`);
+}
+
+render(siteFilmsListContainerElement, createSiteMoreButtonTemplate(), `afterend`);
+
+render(siteFilmsElement, createSiteFilmsExtraTemplate(), `beforeend`);
+const siteFilmsListExraElement = siteFilmsElement.querySelector(`.films-list--extra`);
+const siteFilmsListExraContainerElement = siteFilmsListExraElement.querySelector(`.films-list__container`);
+for (let i = 0; i < NUM_CARDS_OF_EXTRA_FILM; i++) {
+  render(siteFilmsListExraContainerElement, createSiteFilmCardTemplate(), `beforeend`);
+}
+
+const siteFooterStatisticsElement = siteFooterElement.querySelector(`.footer__statistics`);
+render(siteFooterStatisticsElement, createSiteStatisticsTemplate(), `beforeend`);
+render(document.body, createSiteDetailsTemplate(), `beforeend`);
+
+const siteDetailsElement = document.querySelector(`.film-details`);
+siteDetailsElement.classList.add(`visually-hidden`);
---
+++ src/view/statistics.js
@@ -0,0 +1,3 @@
+export const createSiteStatisticsTemplate = () => {
+  return `<p>130 291 movies inside</p>`;
+};
