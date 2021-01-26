ee579563f4402ef3aa0aa1e1760efccb84d267f4
iggyfo <iggyfo@gmail.com>
2021-01-26 23:23:56 +0300

    Удаляет моки Передает загруженные фильмы в модель фильмов

--- src/main.js
+++ src/main.js
@@ -4,14 +4,12 @@ import FilterPresenter from "./presenter/filters";
 import FooterStatisticsView from "./view/footer-statistics";
 import FilmModel from "./model/films";
 import FilterModel from "./model/filters";
-import {generateFilm} from "./mock/film";
 import {render, RenderPosition} from "./utils/render";
 import Api from "./api/api";
 
 const AUTHORIZATION = `Basic jv12n134edvsns`;
 const END_POINT = `https://13.ecmascript.pages.academy/cinemaddict/`;
 
-const NUM_OF_FILMS = 20;
 const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
 const footerElement = document.querySelector(`.footer`);
@@ -19,26 +17,23 @@ const footerElement = document.querySelector(`.footer`);
 
 const api = new Api(END_POINT, AUTHORIZATION);
 
-api.getFilms().then((films) => {
-  console.log(films);
-});
-let films = [];
-
-for (let i = 0; i < NUM_OF_FILMS; i++) {
-  films.push(generateFilm());
-}
 render(headerElement, new MenuView(), RenderPosition.BEFOREEND);
 
 const filtersModel = new FilterModel();
 const filmModel = new FilmModel();
-filmModel.films = films;
+
+api.getFilms().then((films) => {
+  filmModel.films = films;
+});
 
 // presenters
 const filtersPresenter = new FilterPresenter(mainElement, filtersModel, filmModel);
 filtersPresenter.init();
 const filmsList = new FilmsListPresenter(mainElement, filmModel, filtersModel);
-filmsList.init(films);
+filmsList.init();
 
 // footer
 const footerStatisticsElement = footerElement.querySelector(`.footer__statistics`);
-render(footerStatisticsElement, new FooterStatisticsView(films.length), RenderPosition.BEFOREEND);
+// render(footerStatisticsElement, new FooterStatisticsView(filmModel.getFilms.length), RenderPosition.BEFOREEND);
+
+
